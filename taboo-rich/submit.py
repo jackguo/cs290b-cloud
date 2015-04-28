fab88d750cc10d526a715b6d256c56cbf257b9fe
eagle-condor <junsheng.guo90@gmail.com>
2015-04-28 07:22:33 -0500

    added checkpoint on condor

--- taboo-rich/submit.py
+++ taboo-rich/submit.py
@@ -16,18 +16,19 @@ if not os.path.isdir(benchmark):
 os.chdir(benchmark)
 
 max_size = 80
-condor_job = """Universe     = vanilla
+num_of_jobs = 5
+condor_job = """Universe     = standard
 Executable   = %s/simple_taboo_search-7
 Arguments    = %d
 
-Log          = %s/7.out
+Log          = %s/7.log
 Error        = %s/7.err
 Output       = %s/7.out
 
 Queue
 """
 
-for i in range(1, 6):
+for i in range(1, num_of_jobs + 1):
   outdir = "out_max_" + str(max_size) + "_" + str(i)
   os.mkdir(outdir)
   job = condor_job % (exec_dir, max_size + 1, outdir, outdir, outdir)
