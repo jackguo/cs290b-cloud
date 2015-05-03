e64b80d9146d827c4052e2607ee86b860012a21f
Junsheng <junsheng.guo90@gmail.com>
2015-05-03 01:14:24 -0700

    ...

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 # vm setup script: install package
 
 sudo apt-get update
-sudo apt-get -y install emacs24-nox vim git g++
+sudo apt-get -y install emacs24-nox vim git g++ make
 git clone https://github.com/jackguo/emacs-profile.git
 mv emacs-profile .emacs.d
