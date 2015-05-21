e8ef2a80d24beb02b24bc1fd97b36b0f06e4015d
eagle-condor <junsheng.guo90@gmail.com>
2015-05-21 06:22:07 -0500

    ...

--- tabu/submit.py
+++ tabu/submit.py
@@ -20,24 +20,24 @@ max_size = 200
 num_of_jobs = 5
 condor_job = """Universe     = standard
 Executable   = %s/simple_taboo_search-7
-Arguments    = %d 99.ce
+Arguments    = %d 113.ce
 
 should_transfer_files = YES
-transfer_input_files = %s/99.ce
+transfer_input_files = %s/113.ce
 
-Log          = %s/7.log
-Error        = %s/7.err
-Output       = %s/7.out
+Log          = %s/log
+Error        = %s/err
+Output       = %s/out
 
 Queue
 """
 
 for i in range(1, num_of_jobs + 1):
-  outdir = "from_99_out_max_" + str(max_size) + "_" + str(i)
+  outdir = "delay_from_113_out_max_" + str(max_size) + "_" + str(i)
   os.mkdir(outdir)
-  job = condor_job % (exec_dir, max_size + 1, exec_dir, outdir, outdir, outdir)
+  job = condor_job % (exec_dir, max_size + 1, benchmark, outdir, outdir, outdir)
   p1 = Popen(["echo", "-e", job], stdout = PIPE)
   p2 = Popen("condor_submit", stdin = p1.stdout)
   p1.stdout.close()
   p2.communicate()
-  time.sleep(60)
+  time.sleep(90)
